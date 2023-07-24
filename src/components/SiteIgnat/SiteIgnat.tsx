22821304378358516270e71f3fc8c5b060221876
«Elena <LenaRybinskova@gmail.com>
2023-07-24 18:38:46 +0300

    синтаксис useParams

--- src/components/SiteIgnat/SiteIgnat.tsx
+++ src/components/SiteIgnat/SiteIgnat.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import {NavLink, Route, Routes, Outlet} from "react-router-dom";
+import {NavLink, Route, Routes, Outlet, useParams} from "react-router-dom";
+
+
+const Profile=()=>{
+    const params = useParams<"id" | "id2">()
+    const some = params
+    console.log(some)
+    return(
+        <div>profile</div>
+    )
+}
 
 const SiteIgnat = () => {
     return (
-        // 2й вариант вложенности через Чилдрен
         <div>
             <NavLink to={"/"}>main---</NavLink>
             <NavLink to={"/login"}>login---</NavLink>
             <NavLink to={"/propfile"}>profile---</NavLink>
-            <NavLink to={"/propfile/settings"}>settings</NavLink>
+            <NavLink to={"/propfile/1"}>profile/1</NavLink>
 
             <Routes>
                 <Route path={"/*"} element={<div>404</div>}/>
                 <Route path={"/"} element={<div>main</div>}/>
                 <Route path={"/login"} element={<div>login</div>}/>
-                <Route path={"/propfile"} element={
-                    <div>
-                        propfile
-
-                        <Outlet/>
-                    </div>}>
-
-                    <Route path={"*"} element={<div>404 ОШИБКА</div>}/>
-                    <Route path={"settings"} element={<div>settings</div>}/>
-                    <Route path={"music"} element={<div>music</div>}/>
-
+                <Route path={"/propfile/:id/:id2"} element={<Profile/>}>
                 </Route>
             </Routes>
         </div>
@@ -34,7 +33,7 @@ const SiteIgnat = () => {
 export default SiteIgnat;
 
 
-// первый вариант вложенности:
+// первый вариант вложенности:--------------------
 // const SiteIgnat = () => {
 //     return (
 //         <div>
@@ -65,7 +64,7 @@ export default SiteIgnat;
 // export default SiteIgnat;
 
 
-// второй вариант вложенности
+// второй вариант вложенности ---------------
 // const SiteIgnat = () => {
 //     return (
 //         // 2й вариант вложенности через Чилдрен - чилдрен отрисуется вместо Outlet
@@ -87,7 +86,41 @@ export default SiteIgnat;
 //                     </div>}>
 //
 //                     <Route path={"settings"} element={<div>settings</div>}/>
+//                     <Route path={"*"} element={<div>404 ОШИБКА</div>}/>
 //
+//                 </Route>
+//             </Routes>
+//         </div>
+//     );
+// };
+//
+// export default SiteIgnat;
+
+
+
+// useParam
+// const Profile=()=>{
+//     const params = useParams<"id" | "id2">()
+//     const some = params
+//     console.log(some)
+//     return(
+//         <div>profile</div>
+//     )
+// }
+//
+// const SiteIgnat = () => {
+//     return (
+//         <div>
+//             <NavLink to={"/"}>main---</NavLink>
+//             <NavLink to={"/login"}>login---</NavLink>
+//             <NavLink to={"/propfile"}>profile---</NavLink>
+//             <NavLink to={"/propfile/1"}>profile/1</NavLink>
+//
+//             <Routes>
+//                 <Route path={"/*"} element={<div>404</div>}/>
+//                 <Route path={"/"} element={<div>main</div>}/>
+//                 <Route path={"/login"} element={<div>login</div>}/>
+//                 <Route path={"/propfile/:id/:id2"} element={<Profile/>}>
 //                 </Route>
 //             </Routes>
 //         </div>
